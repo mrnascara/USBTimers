a54efe9acd15d6c220927854b08ae4bf4ff15edb
mrnascara <mrnascara@gmail.com>
2026-02-11 03:29:51 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <sys/basicusb.h>
 #include <ti/getkey.h>
 /* Main function, called first */
-int main(void)
+int main(void) 
 {
     /* Clear the homescreen */
     os_ClrHome();
@@ -12,23 +12,30 @@ int main(void)
     /* Print a string */
     os_PutStrFull("This program will disable the usb timers & controllers.");
     os_ClrHome();
+    os_PutStrFull("press any key to continue....");
+   uint16_t os_GetKey(void);
     /*disables usb timers*/
-    void boot_USBDisableTimers(void)
+    void boot_USBDisableTimers(void);
 
     /* notify usb timers are off */
     ;os_PutStrFull("usb timers off,want to turn then on? if so press add key .");
     /*checks if the add key is press,if so then enables usb timers*/
+    uint16_t os_GetKey(void);
+
 if (k_Add)
 {
 void boot_USBEnableTimers(void);
-;os_PutStrFull("usb timers are on");
+;os_PutStrFull("usb timers are on,press any key");
+uint16_t os_GetKey(void);
 os_ClrHome();
+uint16_t os_GetKey(void);
 }
 if (k_Sub)
 {
      void boot_USBResetTimers(void);
      os_ClrHome();
      os_PutStrFull("usb timers reset");
+     uint16_t os_GetKey(void);
 }
 
     return 0;
